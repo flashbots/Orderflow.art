432f3c923f2fa02e79842c26bcbfd4f779959275
Daniel Sukoneck <daniel@sukoneck.com>
2025-07-17 19:41:01 +0100

    add env for optional custom fetch url

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default async function fetcher(uri: any, queryParam: any) {
-  const response = await axios.get(uri + queryParam);
+  const url = process.env.SERVER_URL ? process.env.SERVER_URL + uri + queryParam : uri + queryParam;
+  const response = await axios.get(url);
   return response.data;
 }
