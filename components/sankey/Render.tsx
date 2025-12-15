7cb3edfa854ee1610dd0784677922be94f2cc892
facuzeta <facuzeta@gmail.com>
2025-12-15 13:12:31 -0300

    Moving the banner to the bottom of the site

--- components/sankey/Render.tsx
+++ components/sankey/Render.tsx
@@ -60,7 +60,29 @@ export default function SankeyRender() {
 
   return (
     <>
-<div className="border border-dune-300 bg-dune-200 px-6 py-6 rounded-md text-center space-y-6">
+      <div className="border border-dune-300">
+        <SankeyFilter
+          entityData={entitiesData?.entities}
+          entityFilter={sankeyData?.data?.entityFilter ?? ""}
+          graphType={graphType}
+          setGraphType={setGraphType}
+          isOrderflow={isOrderflow}
+          setQueryParam={setQueryParam}
+          txHash={txHash}
+          setTxHash={setTxHash}
+          timeframe={timeframe}
+          setTimeframe={setTimeframe}
+        />
+        <SankeyGraph
+          height={Math.min(Math.max(windowHeight - 72, 600), 2000)}
+          txHash={txHash}
+          isLoading={sankeyLoading}
+          data={sankeyData?.data}
+          rangeData={rangeData?.data}
+          error={sankeyError}
+        />
+      </div>
+      <div className="border border-dune-300 bg-dune-200 px-6 py-6 rounded-md text-center space-y-6">
 
   {/* Headline */}
   <p className="font-semibold text-lg">
@@ -71,9 +93,9 @@ export default function SankeyRender() {
   {rangeData?.data?.range && (
     <div className="text-left max-w-xl mx-auto text-sm">
       The data displayed on this site covers the period from{" "}
-      <strong>{getDateString(rangeData.data.range.startTime).split(" ")[0]}</strong>{" "}
+      <strong>{getDateString(rangeData.data.range.startTime).split(" ")[0]} ({rangeData.data.range.startBlock})</strong>{" "}
       to{" "}
-      <strong>{getDateString(rangeData.data.range.endTime).split(" ")[0]}</strong>{" "}
+      <strong>{getDateString(rangeData.data.range.endTime).split(" ")[0]} ({rangeData.data.range.endBlock})</strong>{" "}
       and is no longer being updated.
     </div>
   )}
@@ -121,28 +143,8 @@ export default function SankeyRender() {
   </ul>
 </div>
 </div>
-      <div className="border border-dune-300">
-        <SankeyFilter
-          entityData={entitiesData?.entities}
-          entityFilter={sankeyData?.data?.entityFilter ?? ""}
-          graphType={graphType}
-          setGraphType={setGraphType}
-          isOrderflow={isOrderflow}
-          setQueryParam={setQueryParam}
-          txHash={txHash}
-          setTxHash={setTxHash}
-          timeframe={timeframe}
-          setTimeframe={setTimeframe}
-        />
-        <SankeyGraph
-          height={Math.min(Math.max(windowHeight - 72, 600), 2000)}
-          txHash={txHash}
-          isLoading={sankeyLoading}
-          data={sankeyData?.data}
-          rangeData={rangeData?.data}
-          error={sankeyError}
-        />
-      </div>
+
+
     </>
   );
 }
--- utils/clickhouse.ts
+++ utils/clickhouse.ts
@@ -4,4 +4,5 @@ export const client = createClient({
   host: process.env.CLICKHOUSE_HOST ?? "http://localhost:8123",
   username: process.env.CLICKHOUSE_USER ?? "default",
   password: process.env.CLICKHOUSE_PASSWORD ?? "",
+  database: process.env.CLICKHOUSE_DATABASE ?? "default",
 });
